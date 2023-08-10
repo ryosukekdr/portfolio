ea54b7ab24669a2fdfad717231fe265cc75d4039
ryosuke <ryosukekdr@yahoo.co.jp>
2023-08-10 13:48:00 +0000

    pushやり直し

--- resources/views/blog_detail.blade.php
+++ resources/views/blog_detail.blade.php
@@ -5,20 +5,20 @@
         <div class="blog-detail-card">
             <h1 class="detail-title">{{ $post->title }}</h1>
             <div class="row text-align-right">
-                        <div class="kaigyo col-md-6">
-                                    {{ $post->body }}
+                <div class="kaigyo col-md-6">
+                    {{ $post->body }}
+                </div>
+                <div class="detail-image col-md-6">
+                    @if (!($post->images->isEmpty()))  {{--hasmanyリレーションで$postが複数持ってる画像を１枚ずつ表示。$post->images != NULLだと画像無くてもif文通過してしまう。imagesがコレクションのため--}}
+                        <div class="text-align-center">
+                            <br>
+                            @foreach ($post->images as $image)
+                                <img src="{{ secure_asset('storage/image/' . $image->image_path) }}">
+                                <br>
+                            @endforeach
                         </div>
-                        <div class="detail-image col-md-6">
-                                @if (!($post->images->isEmpty()))  {{--hasmanyリレーションで$postが複数持ってる画像を１枚ずつ表示。$post->images != NULLだと画像無くてもif文通過してしまう。imagesがコレクションのため--}}
-                                    <div class="text-align-center">
-                                        <br>
-                                        @foreach ($post->images as $image)
-                                            <img src="{{ secure_asset('storage/image/' . $image->image_path) }}">
-                                            <br>
-                                        @endforeach
-                                    </div>
-                                @endif
-                        </div>
+                    @endif
+                </div>
             </div>
             <div class=detail-footer>
                 <i class="fas fa-edit">{{ date('Y年m月d日', strtotime($post->created_at)) }}</i>
